67358062d53b03c21d85432f00647526b3272fed
kfkzsy <1683709605@qq.com>
2026-08-28 15:55:07 +0800

    fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://127.0.0.1:10081',
+        target: 'http://127.0.0.1:10081',
         
         // target: 'https://drone.kfktec.cn:8443',
-        target: 'http://192.168.1.6:8080',
+        // target: 'http://192.168.1.6:8080',
         changeOrigin: true,
         ws: true,
       }
